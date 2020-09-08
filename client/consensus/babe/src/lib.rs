f53c15b37a8fbf95f33e719d9e1f33fb69e6c1fc
Dengjianping <djptux@gmail.com>
2020-09-08 18:25:41 +0800

    Update comments

--- client/consensus/babe/src/lib.rs
+++ client/consensus/babe/src/lib.rs
@@ -159,7 +159,7 @@ impl EpochT for Epoch {
 		Epoch {
 			epoch_index: self.epoch_index + 1,
 			start_slot: self.start_slot + self.duration,
-//			start_slot: self.start_slot + 300 * 48 * 5 * 2, // catch the epoch by 10 days each round
+//			start_slot: self.start_slot + 300 * 48 * 5 * 2, // catch up the latest epoch by 10 days each round
 			duration: self.duration,
 			authorities: descriptor.authorities,
 			randomness: descriptor.randomness,
